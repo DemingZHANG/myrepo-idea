6169f31e755072ed483997b6add793e436a6706f
DemingZHANG <deming_zhang1@126.com>
2019-03-08 20:48:17 +0800

    a.html create

--- git_day01_demo/src/main/java/com/itheima/domain/User.java
+++ git_day01_demo/src/main/java/com/itheima/domain/User.java
@@ -16,6 +16,11 @@ public class User {
         this.age = age;
     }
 
+    public User(String username, String password) {
+        this.username = username;
+        this.password = password;
+    }
+
     public Integer getAge() {
         return age;
     }
