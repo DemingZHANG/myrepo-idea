b2e5297187da913ee3468600ce5bf323b7537ddd
DemingZHANG <deming_zhang1@126.com>
2019-03-08 20:50:11 +0800

    a.html create

--- git_day01_demo/src/main/java/com/itheima/domain/Customer.java
+++ git_day01_demo/src/main/java/com/itheima/domain/Customer.java
@@ -3,4 +3,12 @@ package com.itheima.domain;
 public class Customer {
 
     private String username;
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
 }
